8fb0d3400c0715821d3ac9c168cfee1ed6b8b239
kamalyesh <kkamalyesh@gmail.com>
2022-11-06 00:20:56 +0530

    :wrench: :hammer:  :heavy_plus_sign: :recycle:

---
+++ index.ts
@@ -0,0 +1,15 @@
+import * as Degree from "./src/Degree";
+import * as ID from "./src/ID";
+import * as Name from "./src/Name";
+import * as Relationships from "./src/Relationships";
+import * as Relatives from "./src/Relatives";
+import * as User from "./src/User";
+
+export default {
+  Degree,
+  ID,
+  Name,
+  Relationships,
+  Relatives,
+  User,
+}
